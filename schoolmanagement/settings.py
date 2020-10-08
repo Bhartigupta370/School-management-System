dc0994883e7d449d75b23bf455422885fe1f7bb6
Bharti Gupta <53184543+Bhartigupta370@users.noreply.github.com>
2020-10-08 16:48:16 +0530

    Update settings.py

--- schoolmanagement/settings.py
+++ schoolmanagement/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'SchoolManagement',
         'USER': 'postgres',
-        'PASSWORD': 'maapaa@me',
+        'PASSWORD': 'password',
         'HOST': 'localhost',
         'PORT': '5432',
     }
